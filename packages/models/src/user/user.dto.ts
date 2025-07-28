d425c57e567b43c5bc4d95dcd5e56c85390c19d0
andlicon <andresjzabalac@hotmail.com>
2025-07-28 09:20:38 -0400

    feat(package): :sparkles: zod validations for string

--- packages/models/src/user/user.dto.ts
+++ packages/models/src/user/user.dto.ts
@@ -1,11 +1,12 @@
 import { z } from 'zod';
-import { basicModelDefinition } from '../utils';
+import { basicModelDefinition, StringZod } from '../utils';
 
 export const userDefinition = basicModelDefinition.extend({
-  username: z.string(),
-  email: z.email(),
-  password: z.string(),
-  name: z.string(),
-  phone: z.string(),
+  username: StringZod({ type: 'text', length: { min: 4, max: 20 } }),
+  email: StringZod({ type: 'email' }),
+  password: StringZod({ type: 'password', length: { min: 8, max: 20 } }),
+  name: StringZod({ type: 'text' }),
+  phone: StringZod({ type: 'phone' }),
   image: z.string(),
 });
+export type IUser = z.infer<typeof userDefinition>;
--- packages/models/src/utils/schemaUtils.ts
+++ packages/models/src/utils/schemaUtils.ts
@@ -7,3 +7,59 @@ export const basicModelDefinition = z.object({
   createdAt: z.date(),
   updatedAt: z.date(),
 });
+
+type StringType = 'text' | 'email' | 'phone' | 'password';
+
+const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const PHONE_REGEX = /^\+?\d{1,3}?[-.\s]?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/;
+const PASSWORD_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~`])(?!.*\s).{8,}$/;
+
+interface ZodStringProps {
+  type: StringType;
+  length?: {
+    min: number;
+    max: number;
+  };
+}
+
+export const StringZod = ({ type, length }: ZodStringProps): z.ZodString => {
+  return z.string().check((ctx) => {
+    const value = ctx.value;
+    if (type === 'email' && !value.match(EMAIL_REGEX)) {
+      ctx.issues.push({
+        code: 'custom',
+        message: 'Email inválido',
+        input: ctx.value,
+      });
+    }
+    if (type === 'phone' && !value.match(PHONE_REGEX)) {
+      ctx.issues.push({
+        code: 'custom',
+        message: 'Número telefónico inválido',
+        input: ctx.value,
+      });
+    }
+    if (type === 'password' && !value.match(PASSWORD_REGEX)) {
+      ctx.issues.push({
+        code: 'custom',
+        message: 'Contraseña inválida',
+        input: ctx.value,
+      });
+    }
+    if (length?.min && value.length < length.min) {
+      ctx.issues.push({
+        code: 'custom',
+        message: `El campo debe tener una longitud de ${length.min} carácteres`,
+        input: ctx.value,
+      });
+    }
+    if (length?.max && value.length > length.max) {
+      ctx.issues.push({
+        code: 'custom',
+        message: `El campo debe tener una longitud de ${length.max} carácteres`,
+        input: ctx.value,
+      });
+    }
+  });
+};
